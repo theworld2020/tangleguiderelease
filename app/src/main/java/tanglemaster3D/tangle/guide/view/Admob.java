4a3160a412cfcdecae11412ec2028226b9b0133b
selva <smileselvakumar@gmail.com>
2020-06-27 13:57:12 +0530

    Test with toast

--- app/src/main/java/tanglemaster3D/tangle/guide/view/Admob.java
+++ app/src/main/java/tanglemaster3D/tangle/guide/view/Admob.java
@@ -1,12 +1,14 @@
 package tanglemaster3D.tangle.guide.view;
 
 import android.app.Activity;
-
+import android.widget.Toast;
 
 
 public class Admob extends Activity {
 
 	protected void showAds() {
+
+		Toast.makeText(this, "testing", Toast.LENGTH_SHORT).show();
 
 	}
 }
