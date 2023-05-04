10a80a15c8b9b9b4124d4143817ef191ad70b7ed
theworld2020 <smileselvakumar@gmail.com>
2023-05-04 16:57:24 +0530

    Ad permission updated in mainifest file

--- app/build/generated/source/buildConfig/release/openai/chatgpt/guide/view/BuildConfig.java
+++ app/build/generated/source/buildConfig/release/openai/chatgpt/guide/view/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "openai.chatgpt.guide.view";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 5;
-  public static final String VERSION_NAME = "1.0.4";
+  public static final int VERSION_CODE = 7;
+  public static final String VERSION_NAME = "2.0.1";
 }
